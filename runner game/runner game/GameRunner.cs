9c50b245fbdcf8e48be4f28da9f3ff8f4870026a
dlwhwhdvlf <zkrkfhtm4341@naver.com>
2024-05-08 07:10:44 +0900

    -all

---
+++ runner game/runner game/GameRunner.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace runner_game
+{
+    public partial class GameRunner : Form
+    {
+        bool jumping = false;
+        int jumpSpeed;
+        int force = 12;
+        int score = 0;
+        int obstacleSpeed = 10;
+        Random rand = new Random();
+        int position;
+        bool isGameOver = false;
+
+        public GameRunner()
+        {
+            InitializeComponent();
+
+            GameReset();
+        }
+        
+        private void GameTimerEvent(object sender, EventArgs e)
+        {
+            pbRunner.Top += jumpSpeed;
+
+            lblScore.Text = "점수: " + score;
+
+            if(jumping == true && force < 0)
+            {
+                jumping = false;
+            }
+
+            if (jumping == true)
+            {
+                jumpSpeed = -12;
+                force -= 1;
+            }
+            else
+            {
+                jumpSpeed = 12;
+            }
+
+            if (pbRunner.Top > 334 && jumping == false)
+            {
+                force = 12;
+                pbRunner.Top = 335;
+                jumpSpeed = 0;
+            }
+
+            foreach(Control x in this.Controls)
+            {
+                if(x is PictureBox && (string)x.Tag == "obstacle")
+                {
+                    x.Left -= obstacleSpeed;
+
+                    if(x.Left < -100)
+                    {
+                        x.Left = this.ClientSize.Width + rand.Next(200, 500) + (x.Width * 15);
+                        score++;
+                    }
+
+                    if(pbRunner.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        gameTimer.Stop();
+                        pbRunner.Image = Properties.Resources.dead;
+                        lblScore.Text += " 다시 시작하려면 R키를 누르세요.";
+                        MessageBox.Show("Game Over!");
+                        isGameOver = true;
+                    }
+                }
+            }
+
+            if (score > 5)
+            {
+                obstacleSpeed = 15;
+            }
+        }
+
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space && jumping == false)
+            {
+                jumping = true;
+            }
+        }
+
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+            if (jumping == true)
+            {
+                jumping = false;
+            }
+
+            if (e.KeyCode == Keys.R && isGameOver == true)
+            {
+                GameReset();
+            }
+        }
+
+        private void GameReset()
+        {
+            force = 12;
+            jumpSpeed = 0;
+            jumping = false;
+            score = 0;
+            obstacleSpeed = 10;
+            lblScore.Text = "점수: " + score;
+            pbRunner.Image = Properties.Resources.running;
+            isGameOver = false;
+            pbRunner.Top = 335;
+
+            foreach (Control x in this.Controls)
+            {
+
+                if (x is PictureBox && (string)x.Tag == "obstacle")
+                {
+                    position = this.ClientSize.Width + rand.Next(500, 800) + (x.Width * 10);
+
+                    x.Left = position;
+                }
+            }
+
+            gameTimer.Start();
+
+        }
+    }
+}
---
+++ runner game/runner game/Main.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace runner_game
+{
+    partial class Game
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblRunner = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(152, 324);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(150, 63);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "시작";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblRunner
+            // 
+            this.lblRunner.AutoSize = true;
+            this.lblRunner.Font = new System.Drawing.Font("굴림", 20F);
+            this.lblRunner.Location = new System.Drawing.Point(252, 160);
+            this.lblRunner.Name = "lblRunner";
+            this.lblRunner.Size = new System.Drawing.Size(262, 34);
+            this.lblRunner.TabIndex = 1;
+            this.lblRunner.Text = "RUNNER GAME";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(463, 324);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(150, 63);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "종료";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.lblRunner);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Game";
+            this.Text = "Main";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblRunner;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
